a4e32d36f04cb71911b485e85087251a63c90a4c
junjo255 <jun.jo249@gmail.com>
2019-02-07 09:50:42 -0500

    updated database

--- server/db/index.js
+++ server/db/index.js
@@ -1,31 +1,84 @@
 var sqlite3 = require('sqlite3').verbose();
 var faker = require('faker');
+var moment = require('moment');
 
 var db = new sqlite3.Database('massdrop.db')
 
 //
+db.serialize(function(){
+    
+    var product = db.prepare("INSERT INTO product VALUES (?,?)");
+    product.run(1, "nike sneaker")
+    product.finalize()
+})
 
-db.serialize(function() {
+db.serialize(function () {
     var user = db.prepare("INSERT INTO user VALUES (?,?,?)");
 
-    for (var i = 0 ; i < 10 ; i++){
+    for (var i = 0; i < 10; i++) {
 
-        user.run(faker.internet.userName(), faker.image.avatar())
+        user.run(Math.floor(Math.random(i) * 1000), faker.internet.userName() , faker.image.avatar())
     }
-    user.finalize();
+    user.finalize(
 
+    );
 
 })
-// db.serialize(function() {
-//     db.run("CREATE TABLE lorem (info TEXT)");
-   
-//     var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-//     for (var i = 0; i < 10; i++) {
-//         stmt.run("Ipsum " + i);
-//     }
-//     stmt.finalize();
-   
-//   });
+
+
+db.serialize(function () {
+    var thread = db.prepare("INSERT INTO thread VALUES(?,?)");
+    for (var i = 0; i < 3; i++) {
+        thread.run(Math.floor(Math.random(i) * 1000), faker.lorem.word())
+    }
+    thread.finalize();
+})
+
+
+db.serialize(function () {
+    var users = db.prepare("SELECT id FROM user ")
+    var messages = db.prepare("INSERT INTO messages VALUES(?,?,?,?,?)");
+    var threads = db.prepare("SELECT id FROM thread")
+    // console.log(typeof threads, "hello type of threads")
+
+    function threadsf() {
+        var result = []
+        for (var key in threads) {
+            result.push(threads[key])
+
+        }
+        result = result.forEach(id => Math.floor(Math.random(id) * 10))
+        return result;
+    }
+
+    function usersf() {
+        var result = []
+        for (var key in users) {
+            result.push(users[key])
+
+        }
+        result = result.forEach(id => Math.floor(Math.random(id) * 10))
+        return result
+    }
+    // var result = threads().forEach(id => Math.floor(Math.random(id) * 10))
+
+    // function usersf() {
+    //     var result = users.forEach(id => Math.floor(Math.random(id) * 10))
+    //     return result;
+    // }
+
+    for (var i = 0; i < 100; i++) {
+        messages.run(
+            (Math.floor(Math.random(i) * 1000)),
+            (threadsf()),
+            (faker.date.past()),
+            (faker.lorem.paragraph()),
+            (usersf()))
+    }
+
+    messages.finalize();
+})
+
 
 
 
--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,38 +3,39 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS 'product';
 CREATE TABLE `product` (
-  -- `id` INTEGER AUTO_INCREMENT DEFAULT 'NULL',
-  `product_name` VARCHAR(200) NOT NULL DEFAULT 'NULL'
-  -- PRIMARY KEY (`id`)
+  `id` INTEGER AUTO_INCREMENT DEFAULT 'NULL',
+  `product_name` VARCHAR(200) NOT NULL DEFAULT 'NULL',
+  PRIMARY KEY (`id`)
 );
 
 DROP TABLE IF EXISTS 'user';
 
 CREATE TABLE `user` (
-  -- `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
+  `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
  
   `username` VARCHAR(200) NULL DEFAULT NULL,
-  `avatar` BLOB NULL DEFAULT NULL
-  -- PRIMARY KEY (`id`)
+  `avatar` BLOB NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
 
 DROP TABLE IF EXISTS `product/threads`;
 
 CREATE TABLE `product/threads` (
-  -- `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
+  `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
   `product_id` INTEGER NULL DEFAULT NULL,
-  `thread_id` INTEGER NULL DEFAULT NULL
-  -- PRIMARY KEY (`id`)
-    FOREIGN KEY (product_id) REFERENCES `product` (`id`),
-  FOREIGN KEY (thread_id) REFERENCES `thread` (`id`)
+  `thread_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+  --   FOREIGN KEY (product_id) REFERENCES `product` (`id`),
+  -- FOREIGN KEY (thread_id) REFERENCES `thread` (`id`)
 );
 
 DROP TABLE IF EXISTS `thread`;
 
 CREATE TABLE `thread` (
-  -- `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
-  -- PRIMARY KEY (`id`)
+  `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
+  `thread_name` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 
 );
 
@@ -42,25 +43,25 @@ CREATE TABLE `thread` (
 DROP TABLE IF EXISTS `thread discussion participants`;
 
 CREATE TABLE `thread discussion participants` (
-  -- `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
+  `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
   `thread_id` INTEGER NULL DEFAULT NULL,
-  `user_id` INTEGER NULL DEFAULT NULL
-  -- PRIMARY KEY (`id`),
-  FOREIGN KEY (thread_id) REFERENCES `thread` (`id`),
-  FOREIGN KEY (user_id) REFERENCES `user` (`id`)
+  `user_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+  -- FOREIGN KEY (thread_id) REFERENCES `thread` (`id`),
+  -- FOREIGN KEY (user_id) REFERENCES `user` (`id`)
 );
 
 
 DROP TABLE IF EXISTS `messages`;
 
 CREATE TABLE `messages` (
-  -- `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
+  `id` INTEGER AUTO_INCREMENT DEFAULT NULL,
   `thread_id` INTEGER NULL DEFAULT NULL,
   `createdAt` DATETIME NULL DEFAULT NULL,
   `body` VARCHAR(1000) NULL DEFAULT NULL,
   `sending_user_id` INTEGER NULL DEFAULT NULL,
-  -- PRIMARY KEY (`id`),
-  FOREIGN KEY (thread_id) REFERENCES `thread` (`id`),
-  FOREIGN KEY (sending_user_id) REFERENCES `user` (`id`)
+  PRIMARY KEY (`id`)
+  -- FOREIGN KEY (thread_id) REFERENCES `thread` (`id`),
+  -- FOREIGN KEY (sending_user_id) REFERENCES `user` (`id`)
 );
 COMMIT;
